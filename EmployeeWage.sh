400e9f7ad20e7a6eea61d0d2cec4e176e5489823
Bhushan Saundale <saundalebhushan5@gmail.com>
2020-03-23 15:04:00 +0530

    [BHUSHAN]Added: Check Employee Monthly Salary Code.

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -3,24 +3,27 @@ echo "Welcome to Employee Wage Computation Program."
 
 isPartTime=1;
 isFullTime=2;
-empRatePerHr=20;
-empCheck=$((RANDOM%3));
-     case  $empCheck in
+totalSalary=0;
+empRatePerHrs=20;
+numWorkingDays=20;
 
-               $isFullTime)
-                         empHrs=8;
-echo "Employee is FullTime."
-                         ;;
-                $isPartTime)
-                          empHrs=4;
-echo "Employee is PartTime."
-                          ;;
-                 *)
-               empHrs=0;
-echo "Absent Employee."
-                          ;;
-    esac
+for (( day=1; day<=$numWorkingDays; day++ ))
+   do
+      empCheck=$((RANDOM%3));
+     case $empCheck in
+       $isFullTime)
+				empHrs=8;
+               ;;
+       $isPartTime)
+              empHrs=4;
+                ;;
+           *)
+         empHrs=0;
+                 ;;
+      esac
+     salary=$(($empHrs * $empRatePerHrs));
 
-    salary=$(( $empHrs * $empRatePerHr ));
-echo "Salary=" $salary
-
+     totalSalary=$(($totalSalary + $salary ))
+done
+echo "totalSalary=" $totalSalary
+    
