8918c29f3ed5f49f13b0da4d2ecb290b1e4d9364
Bhushan Saundale <saundalebhushan5@gmail.com>
2020-03-23 15:59:44 +0530

    [BHUSHAN]Added: Check Employee Daily Salary Along With Month Salary Using Array.

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,39 +1,43 @@
 #!/bin/bash 
 echo "Welcome to Employee Wage Computation Program."
 
-isPartTime=1;
-isFullTime=2;
-totalEmpHrs=0;
-empRatePerHrs=20;
-monthlyHrs=100;
-numWorkingDays=20;
-totalWorkingDays=0;
+isPartTime=1
+isFullTime=2
+totalEmpHrs=0
+empRatePerHrs=20
+monthlyHrs=4
+numWorkingDays=20
+totalWorkingDays=0
 
-function  showWorkingDays() {
-    case $empCheck in
+function  getWorkingDays() {
+		case $1 in
               $isFullTime)
-                empHrs=8;
+                empHrs=8
                  ;;
              $isPartTime)
-               empHrs=4;
+               empHrs=4
                 ;;
             *)
-          empHrs=0;
+          empHrs=0
                 ;;
      esac
      echo $empHrs
    }
-
+ function calDailyWage() {
+	local empHrs=$1
+	wage=$(($empHrs*$empRatePerHrs))
+	echo $wage
+}
 
 while [[ $totalEmpHrs -lt $monthlyHrs && $totalWorkingDays -lt $numWorkingDays ]]
 do
-   (( totalWorkingDays++))
-   empCheck=$((RANDOM%3));
-     empHrs="$( showWorkingDays $empCheck )"
+     ((totalWorkingDays++))
+     empHrs="$( getWorkingDays $((RANDOM%3)) )"
      totalEmpHrs=$(($totalEmpHrs+$empHrs))
+     empDailyWage["$totalWorkingDays"]="$( calDailyWage $empHrs )"
 done
 
-totalSalary=$(($totalEmpHrs*$empRatePerHrs))
+totalSalary="$( calDailyWage $totalEmpHrs )"
 
-echo "Employee Hours" $totalEmpHrs
-echo "Salary " $totalSalary
+echo "Daily Wage " ${empDailyWage[@]}
+
