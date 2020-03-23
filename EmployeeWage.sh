188ba32f03f3407ada745fc3acedcc5079ef7b17
Bhushan Saundale <saundalebhushan5@gmail.com>
2020-03-23 15:43:49 +0530

    [BHUSHAN]Added: Check Employee Work Hours and Salary for a Month Using Function Code.

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -5,29 +5,35 @@ isPartTime=1;
 isFullTime=2;
 totalEmpHrs=0;
 empRatePerHrs=20;
-monthly_Hrs=100;
+monthlyHrs=100;
 numWorkingDays=20;
 totalWorkingDays=0;
 
-while [[ $totalEmpHrs -lt $monthly_Hrs && $totalWorkingDays -lt $numWorkingDays ]]
-
-do
-      (( totalWorkingDays++))
-	    empCheck=$((RANDOM%3));
-       
-          case $empCheck in
+function  showWorkingDays() {
+    case $empCheck in
               $isFullTime)
-                   empHrs=8;
-                   ;;
-               $isPartTime)
-                    empHrs=4;
-                     ;;
+                empHrs=8;
+                 ;;
+             $isPartTime)
+               empHrs=4;
+                ;;
             *)
           empHrs=0;
-                     ;;
-          esac
-        totalEmpHrs=$(( $totalEmpHrs + $empHrs ));
- done
-      totalSalary=$(( $totalEmpHrs + $empRatePerHrs ));
-echo "Employee work Hours " $totalEmpHrs
-echo "Total Salary=" $totalSalary
+                ;;
+     esac
+     echo $empHrs
+   }
+
+
+while [[ $totalEmpHrs -lt $monthlyHrs && $totalWorkingDays -lt $numWorkingDays ]]
+do
+   (( totalWorkingDays++))
+   empCheck=$((RANDOM%3));
+     empHrs="$( showWorkingDays $empCheck )"
+     totalEmpHrs=$(($totalEmpHrs+$empHrs))
+done
+
+totalSalary=$(($totalEmpHrs*$empRatePerHrs))
+
+echo "Employee Hours" $totalEmpHrs
+echo "Salary " $totalSalary
