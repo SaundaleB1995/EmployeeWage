8cf6c9446318757895fcfdd06cb07ae726fb72e0
Bhushan Saundale <saundalebhushan5@gmail.com>
2020-03-23 15:19:15 +0530

    [BHUSHAN]Added: Calculate Work Hours and Salary for a Month Code.

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -3,27 +3,31 @@ echo "Welcome to Employee Wage Computation Program."
 
 isPartTime=1;
 isFullTime=2;
-totalSalary=0;
+totalEmpHrs=0;
 empRatePerHrs=20;
+monthly_Hrs=100;
 numWorkingDays=20;
+totalWorkingDays=0;
 
-for (( day=1; day<=$numWorkingDays; day++ ))
-   do
-      empCheck=$((RANDOM%3));
-     case $empCheck in
-       $isFullTime)
-				empHrs=8;
-               ;;
-       $isPartTime)
-              empHrs=4;
-                ;;
-           *)
-         empHrs=0;
-                 ;;
-      esac
-     salary=$(($empHrs * $empRatePerHrs));
+while [[ $totalEmpHrs -lt $monthly_Hrs && $totalWorkingDays -lt $numWorkingDays ]]
 
-     totalSalary=$(($totalSalary + $salary ))
-done
-echo "totalSalary=" $totalSalary
-    
+do
+      (( totalWorkingDays++))
+	    empCheck=$((RANDOM%3));
+       
+          case $empCheck in
+              $isFullTime)
+                   empHrs=8;
+                   ;;
+               $isPartTime)
+                    empHrs=4;
+                     ;;
+            *)
+          empHrs=0;
+                     ;;
+          esac
+        totalEmpHrs=$(( $totalEmpHrs + $empHrs ));
+ done
+      totalSalary=$(( $totalEmpHrs + $empRatePerHrs ));
+echo "Employee work Hours " $totalEmpHrs
+echo "Total Salary=" $totalSalary
